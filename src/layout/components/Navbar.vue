e5a2ee56815d4422c4fb22b6c1f454570528a12e
chocho <849890769@qq.com>
2025-06-28 02:51:10 +0800

    feat: 优化Navbar.vue右侧功能区

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,12 +7,23 @@
     />
     <breadcrumb class="breadcrumb-container" />
     <div class="right-menu">
+      <el-tooltip content="Under development" effect="dark" placement="bottom">
+        <div class="right-menu-item hover-effect">
+          <el-icon>
+            <i-ep-Search />
+          </el-icon>
+        </div>
+      </el-tooltip>
+      <el-tooltip content="Under development" effect="dark" placement="bottom">
+        <div class="right-menu-item hover-effect">
+          <el-icon>
+            <i-ep-Setting />
+          </el-icon>
+        </div>
+      </el-tooltip>
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
           <img :src="ctx.userInfo.avatar+'?imageView2/1/w/80/h/80'" class="user-avatar" />
-          <el-icon class="el-icon-caret-bottom">
-            <i-ep-MoreFilled />
-          </el-icon>
         </div>
         <template v-slot:dropdown>
           <el-dropdown-menu class="user-dropdown">
@@ -22,9 +33,6 @@
             <a href="https://github.com/chocho-1115/vue-admin" target="_blank">
               <el-dropdown-item>Github</el-dropdown-item>
             </a>
-            <!-- <a href="https://panjiachen.github.io/vue-element-admin-site/#/" target="_blank">
-              <el-dropdown-item>Docs</el-dropdown-item>
-            </a>-->
             <el-dropdown-item @click.native="onLogout" divided>
               <span style="display:block;">Log Out</span>
             </el-dropdown-item>
@@ -65,79 +73,65 @@ const onLogout = async () => {
 
 <style lang="scss" scoped>
 .navbar {
-    height: 50px;
-    overflow: hidden;
-    position: relative;
-    background: #fff;
-    box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
 
-    .hamburger-container {
-        line-height: 46px;
-        height: 100%;
-        float: left;
-        cursor: pointer;
-        transition: background 0.3s;
-        -webkit-tap-highlight-color: transparent;
+  .hamburger-container {
+      line-height: 46px;
+      height: 100%;
+      float: left;
+      cursor: pointer;
+      transition: background 0.3s;
+      -webkit-tap-highlight-color: transparent;
+      &:hover {
+          background: rgba(0, 0, 0, 0.025);
+      }
+  }
 
-        &:hover {
-            background: rgba(0, 0, 0, 0.025);
-        }
+  .breadcrumb-container {
+      float: left;
+  }
+  .right-menu {
+    float: right;
+    height: 100%;
+    line-height: 50px;
+    margin-right: 15px;
+    &:focus {
+        outline: none;
     }
+    .right-menu-item {
+      display: inline-flex;
+      padding: 0 8px;
+      height: 100%;
+      font-size: 22px;
+      color: #888;
+      // vertical-align: text-bottom;
+      align-items: center;
+      &.hover-effect {
+          cursor: pointer;
+          transition: background 0.3s;
 
-    .breadcrumb-container {
-        float: left;
+          &:hover {
+              background: rgba(0, 0, 0, 0.025);
+          }
+      }
     }
-
-    .right-menu {
-        float: right;
-        height: 100%;
-        line-height: 50px;
-
-        &:focus {
-            outline: none;
+    .avatar-container{
+      margin-left: 8px;
+      .avatar-wrapper {
+        margin-top: 5px;
+        .user-avatar {
+            cursor: pointer;
+            display:block;
+            width: 40px;
+            height: 40px;
+            border-radius: 10px;
         }
-
-        .right-menu-item {
-            display: inline-block;
-            padding: 0 8px;
-            height: 100%;
-            font-size: 18px;
-            color: #5a5e66;
-            vertical-align: text-bottom;
-
-            &.hover-effect {
-                cursor: pointer;
-                transition: background 0.3s;
-
-                &:hover {
-                    background: rgba(0, 0, 0, 0.025);
-                }
-            }
-        }
-
-        .avatar-container {
-            margin-right: 30px;
-
-            .avatar-wrapper {
-                margin-top: 5px;
-                position: relative;
-
-                .user-avatar {
-                    cursor: pointer;
-                    width: 40px;
-                    height: 40px;
-                    border-radius: 10px;
-                }
-
-                .el-icon-caret-bottom {
-                    cursor: pointer;
-                    position: absolute;
-                    right: -20px;
-                    top: 25px;
-                    font-size: 12px;
-                }
-            }
-        }
+      }
     }
+  }
 }
 </style>
